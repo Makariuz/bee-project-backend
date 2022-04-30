9f3dc195e2b419e274ac1c38fbb76f1ceacc3f7f
Makariuz <samuel.eduardo23@hotmail.com>
2022-04-30 12:17:22 +0100

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res) => {
-    res.send('Hello world'!)
+    res.send('Hello world!!!!')
 })
 
 const authRoutes = require('./routes/auth.routes')
